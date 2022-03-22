3b12c1338a0660babacc4fb1d12926750179ed2a
Shoyombo Raphael <shoyomboraphaeld@gmail.com>
2022-03-23 00:57:28 +0100

    wrote a POST test

--- task/tests.py
+++ task/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+from rest_framework.test import APIRequestFactory
+
+#   post request test
+factory = APIRequestFactory()
+request = factory.post('api/', {'name': 'do the dishes', 'body': 'quickly o the dishes before moving onto the next thing'})
